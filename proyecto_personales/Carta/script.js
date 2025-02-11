ed2a60931a239b0ee5d26eba09693d58c366d7ac
sentinelnn <187855@upslp.edu.mx>
2025-02-10 21:44:57 -0600

    Update script.js

--- proyecto_personales/Carta/script.js
+++ proyecto_personales/Carta/script.js
@@ -1,5 +1,5 @@
 // Fecha objetivo
-const targetDate = new Date('2024-06-17T21:00:00').getTime();
+const targetDate = new Date('2025-02-14T13:00:00').getTime();
 
 // Actualiza la cuenta regresiva cada segundo
 const countdown = setInterval(() => {
